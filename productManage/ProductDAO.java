bf6107a8f7ac97c85daff65813e29df0d4bc6447
Raksha <144310981+Rakshalokesh@users.noreply.github.com>
2025-05-30 10:27:15 +0530

    Add files via upload

---
+++ productManage/ProductDAO.java
@@ -0,0 +1,12 @@
+package productManage;
+
+	import java.sql.*;
+	import java.util.*;
+	import model.Product; // make sure model.Product exists
+
+	public class ProductDAO {
+	    // Full code from earlier...
+	}
+
+
+
